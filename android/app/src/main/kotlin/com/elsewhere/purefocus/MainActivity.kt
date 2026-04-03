776b9d88cf43d64da224a0dfd0f2a548b15d3caa
Elsewhere <elsewherestudiios.contact@gmail.com>
2026-04-03 17:14:38 +0100

    fix: switch to Java MainActivity, remove Kotlin

--- android/app/src/main/kotlin/com/elsewhere/purefocus/MainActivity.kt
+++ android/app/src/main/kotlin/com/elsewhere/purefocus/MainActivity.kt
@@ -2,4 +2,13 @@ package com.elsewhere.purefocus
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
+EOFl
+cat > android/app/src/main/kotlin/com/elsewhere/purefocus/MainActivity.kt << 'EOF'
+package com.elsewhere.purefocus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
